b59f14da61e1af1eaeaa58eb748d9193dc52ce11
zbb <2427588665@qq.com>
2017-11-20 10:29:56 +0800

    vp

--- app/src/main/java/com/bawei/zhangbinbin20171120/MainActivity.java
+++ app/src/main/java/com/bawei/zhangbinbin20171120/MainActivity.java
@@ -1,38 +1,89 @@
 package com.bawei.zhangbinbin20171120;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bawei.zhangbinbin20171120.bean.MyBean;
 import com.bawei.zhangbinbin20171120.presenter.PlayPresenter;
 import com.bawei.zhangbinbin20171120.utils.Api;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.List;
-import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements PlayContrast.IView{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements PlayContrast.IView {
+
+    @BindView(R.id.vp)
+    ViewPager vp;
+    @BindView(R.id.btn_huan)
+    Button btnHuan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         //http://api.svipmovie.com/front/columns/getVideoList.do?catalogId=402834815584e463015584e539330016&pnum=33
         PlayPresenter playPresenter = new PlayPresenter(this);
-        playPresenter.LoadData(Api.HOST,Api.catalogId,50);
+        playPresenter.LoadData(Api.HOST, Api.catalogId, 50);
     }
 
     @Override
-    public void ShowData(List<MyBean.RetBean.ListBean> list) {
+    public void ShowData(final List<MyBean.RetBean.ListBean> list) {
         //Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
+        vp.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return list.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view==object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                MyBean.RetBean.ListBean lb = list.get(position);
+                View v = View.inflate(MainActivity.this,R.layout.item,null);
+                ImageView img = (ImageView) v.findViewById(R.id.img);
+                TextView title = (TextView) v.findViewById(R.id.tv_title);
+                TextView tvt = (TextView) v.findViewById(R.id.tv_text);
+                title.setText(lb.getTitle());
+                tvt.setText(lb.getDescription());
+                ImageLoader.getInstance().displayImage(lb.getPic(),img);
+                container.addView(v);
+                return v;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        });
 
     }
 
     @Override
     public void ShowError(String e) {
-        Toast.makeText(this, ""+e, Toast.LENGTH_SHORT).show();
-        Log.e("及哈哈哈哈哈哈哈哈哈哈哈",e);
+        Toast.makeText(this, "" + e, Toast.LENGTH_SHORT).show();
+        Log.e("及哈哈哈哈哈哈哈哈哈哈哈", e);
+    }
+
+    @OnClick(R.id.btn_huan)
+    public void onViewClicked() {
     }
 
    /* private int getNextPage() {
