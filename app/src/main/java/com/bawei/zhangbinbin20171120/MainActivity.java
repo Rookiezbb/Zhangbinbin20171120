4f6a80549078f871558f06c39861258ab92dcd15
zbb <2427588665@qq.com>
2017-11-20 09:34:30 +0800

    jk

--- app/src/main/java/com/bawei/zhangbinbin20171120/MainActivity.java
+++ app/src/main/java/com/bawei/zhangbinbin20171120/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //http://api.svipmovie.com/front/columns/getVideoList.do?catalogId=402834815584e463015584e539330016&pnum=33
 
     }
 }
